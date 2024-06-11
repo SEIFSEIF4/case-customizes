039d12424ca0c6df1aebbd85554643d0c75eba11
Seif Elesllam <seifsaif151@gmail.com>
2024-06-11 10:25:02 +0300

    PWA Support

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,10 +2,12 @@ import { type ClassValue, clsx } from "clsx";
 import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
+// Utility function to merge class names
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// Utility function to format price
 export const formatPrice = (price: number) => {
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -15,16 +17,19 @@ export const formatPrice = (price: number) => {
   return formatter.format(price);
 };
 
+// Function to construct metadata for the page
 export function constructMetadata({
   title = "Case Customization",
   description = "Create custom high-quality phone cases in seconds",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
+  manifest = "/manifest.json",
 }: {
   title?: string;
   description?: string;
   image?: string;
   icons?: string;
+  manifest?: string;
 } = {}): Metadata {
   return {
     title,
@@ -41,6 +46,7 @@ export function constructMetadata({
       images: [image],
     },
     icons,
-    metadataBase: new URL("https://case-customizes.com"),
+    manifest,
+    metadataBase: new URL("https://case-customizes.vercel.app"),
   };
 }
